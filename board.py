926b44033e0b5e85343b609c299211e362f557df
Griffin Going <GriffinGoing@gmail.com>
2020-09-25 21:55:36 -0400

    add wall adjacency removals. must fix bug in which elements already removed are attempted to remove via redundant but safe logic.

--- board.py
+++ board.py
@@ -79,12 +79,299 @@ def addWalls():
         temp = graph[2][row]
         temp.walls[directions.WEST] = True
 
+    for row in range(9, 12):
+        temp = graph[1][row]
+        temp.walls[directions.EAST] = True
+        temp = graph[2][row]
+        temp.walls[directions.WEST] = True
+
+    for row in range(2, 5):
+        temp = graph[3][row]
+        temp.walls[directions.EAST] = True
+        temp = graph[4][row]
+        temp.walls[directions.WEST] = True
+
+    for row in range(11, 15):
+        temp = graph[3][row]
+        temp.walls[directions.EAST] = True
+        temp = graph[4][row]
+        temp.walls[directions.WEST] = True
+
+    for row in range(1, 2):
+        temp = graph[5][row]
+        temp.walls[directions.EAST] = True
+        temp = graph[6][row]
+        temp.walls[directions.WEST] = True
+
+    for row in range(4, 8):
+        temp = graph[5][row]
+        temp.walls[directions.EAST] = True
+        temp = graph[6][row]
+        temp.walls[directions.WEST] = True
+
+    for row in range(9, 11):
+        temp = graph[5][row]
+        temp.walls[directions.EAST] = True
+        temp = graph[6][row]
+        temp.walls[directions.WEST] = True
+
+    for row in range(12, 14):
+        temp = graph[5][row]
+        temp.walls[directions.EAST] = True
+        temp = graph[6][row]
+        temp.walls[directions.WEST] = True
+
+    for row in range(0, 3):
+        temp = graph[7][row]
+        temp.walls[directions.EAST] = True
+        temp = graph[8][row]
+        temp.walls[directions.WEST] = True
+
+    for row in range(12, 16):
+        temp = graph[7][row]
+        temp.walls[directions.EAST] = True
+        temp = graph[8][row]
+        temp.walls[directions.WEST] = True
+
+    for row in range(1, 2):
+        temp = graph[9][row]
+        temp.walls[directions.EAST] = True
+        temp = graph[10][row]
+        temp.walls[directions.WEST] = True
+
+    for row in range(6, 9):
+        temp = graph[9][row]
+        temp.walls[directions.EAST] = True
+        temp = graph[10][row]
+        temp.walls[directions.WEST] = True
+
+    for row in range(12, 13):
+        temp = graph[10][row]
+        temp.walls[directions.EAST] = True
+        temp = graph[11][row]
+        temp.walls[directions.WEST] = True
+
+    for row in range(14, 16):
+        temp = graph[10][row]
+        temp.walls[directions.EAST] = True
+        temp = graph[11][row]
+        temp.walls[directions.WEST] = True
+
+    for row in range(2, 4):
+        temp = graph[11][row]
+        temp.walls[directions.EAST] = True
+        temp = graph[12][row]
+        temp.walls[directions.WEST] = True
+
+    for row in range(4, 6):
+        temp = graph[13][row]
+        temp.walls[directions.EAST] = True
+        temp = graph[14][row]
+        temp.walls[directions.WEST] = True
+
+    for row in range(10, 12):
+        temp = graph[13][row]
+        temp.walls[directions.EAST] = True
+        temp = graph[14][row]
+        temp.walls[directions.WEST] = True
+
+    for col in range(4, 6):
+        temp = graph[col][1]
+        temp.walls[directions.SOUTH] = True
+        temp = graph[col][2]
+        temp.walls[directions.NORTH] = True
+
+    for col in range(10, 12):
+        temp = graph[col][1]
+        temp.walls[directions.SOUTH] = True
+        temp = graph[col][2]
+        temp.walls[directions.NORTH] = True
+
+    for col in range(2, 4):
+        temp = graph[col][3]
+        temp.walls[directions.SOUTH] = True
+        temp = graph[col][4]
+        temp.walls[directions.NORTH] = True
+
+    for col in range(6, 7):
+        temp = graph[col][3]
+        temp.walls[directions.SOUTH] = True
+        temp = graph[col][4]
+        temp.walls[directions.NORTH] = True
+
+    for col in range(10, 14):
+        temp = graph[col][3]
+        temp.walls[directions.SOUTH] = True
+        temp = graph[col][4]
+        temp.walls[directions.NORTH] = True
+
+    for col in range(7, 12):
+        temp = graph[col][5]
+        temp.walls[directions.SOUTH] = True
+        temp = graph[col][6]
+        temp.walls[directions.NORTH] = True
+
+    for col in range(14, 15):
+        temp = graph[col][5]
+        temp.walls[directions.SOUTH] = True
+        temp = graph[col][6]
+        temp.walls[directions.NORTH] = True
+
+    for col in range(2, 4):
+        temp = graph[col][6]
+        temp.walls[directions.SOUTH] = True
+        temp = graph[col][7]
+        temp.walls[directions.NORTH] = True
+
+    for col in range(12, 16):
+        temp = graph[col][7]
+        temp.walls[directions.SOUTH] = True
+        temp = graph[col][8]
+        temp.walls[directions.NORTH] = True
+
+    for col in range(0, 4):
+        temp = graph[col][8]
+        temp.walls[directions.SOUTH] = True
+        temp = graph[col][9]
+        temp.walls[directions.NORTH] = True
+
+    for col in range(6, 8):
+        temp = graph[col][9]
+        temp.walls[directions.SOUTH] = True
+        temp = graph[col][10]
+        temp.walls[directions.NORTH] = True
+
+    for col in range(9, 12):
+        temp = graph[col][9]
+        temp.walls[directions.SOUTH] = True
+        temp = graph[col][10]
+        temp.walls[directions.NORTH] = True
+
+    for col in range(14, 15):
+        temp = graph[col][9]
+        temp.walls[directions.SOUTH] = True
+        temp = graph[col][10]
+        temp.walls[directions.NORTH] = True
+
+    for col in range(2, 3):
+        temp = graph[col][11]
+        temp.walls[directions.SOUTH] = True
+        temp = graph[col][12]
+        temp.walls[directions.NORTH] = True
+
+    for col in range(10, 14):
+        temp = graph[col][11]
+        temp.walls[directions.SOUTH] = True
+        temp = graph[col][12]
+        temp.walls[directions.NORTH] = True
+
+    for col in range(4, 6):
+        temp = graph[col][13]
+        temp.walls[directions.SOUTH] = True
+        temp = graph[col][14]
+        temp.walls[directions.NORTH] = True
+
+    for col in range(8, 9):
+        temp = graph[col][13]
+        temp.walls[directions.SOUTH] = True
+        temp = graph[col][14]
+        temp.walls[directions.NORTH] = True
 
 
+def removeWallAdjacencies():
+    for col in range(cols):
+        for row in range(rows):
+            temp = graph[col][row]
+
+            #north, nw, ne, and double-north-wall adjacencies
+            if (temp.walls[directions.NORTH] and row > 0):
+                #print(graph[col][row-1].name)
+                nodeToRemove = graph[col][row-1]
+                temp.adjacentTo.remove(nodeToRemove)
+
+                #ne corner
+                removeNE = (temp.walls[directions.EAST] and col < 15) or (col < 15 and graph[col+1][row].walls[directions.NORTH])
+                if (removeNE):
+                    nodeToRemove = graph[col+1][row-1]
+                    temp.adjacentTo.remove(nodeToRemove)
+
+                #nw corner
+                removeNW = (temp.walls[directions.WEST] and col > 0) or (col > 0 and graph[col - 1][row].walls[directions.NORTH])
+                if (removeNW):
+                    nodeToRemove = graph[col - 1][row - 1]
+                    temp.adjacentTo.remove(nodeToRemove)
+
+
+            #south, sw, se, and double-south-wall adjacencies
+            if (temp.walls[directions.SOUTH] and row < 15):
+                #print(graph[col][row-1].name)
+                nodeToRemove = graph[col][row+1]
+                temp.adjacentTo.remove(nodeToRemove)
+
+                #se corner
+                removeSE = (temp.walls[directions.EAST] and col < 15) or (col < 15 and graph[col+1][row].walls[directions.SOUTH])
+                if (removeSE):
+                    nodeToRemove = graph[col+1][row+1]
+                    temp.adjacentTo.remove(nodeToRemove)
+
+                #sw corner
+                removeSW = (temp.walls[directions.WEST] and col > 0) or (col > 0 and graph[col - 1][row].walls[directions.SOUTH])
+                if (removeSW):
+                    nodeToRemove = graph[col-1][row+1]
+                    temp.adjacentTo.remove(nodeToRemove)
+
+                #east and double-east-walls
+                if (temp.walls[directions.EAST] and col < 15):
+                    # print(graph[col][row-1].name)
+                    nodeToRemove = graph[col+1][row]
+                    temp.adjacentTo.remove(nodeToRemove)
+
+                    #ne
+                    if (row > 0 and graph[col][row-1].walls[directions.EAST]):
+                        nodeToRemove = graph[col+1][row-1]
+                        if (): temp.adjacentTo.remove(nodeToRemove)
+
+                    #se
+                    if (row < 15 and graph[col][row+1].walls[directions.EAST]):
+                        nodeToRemove = graph[col+1][row+1]
+                        temp.adjacentTo.remove(nodeToRemove)
+
+                #west and double-west-walls
+                if (temp.walls[directions.WEST] and col > 0):
+                    # print(graph[col][row-1].name)
+                    nodeToRemove = graph[col-1][row]
+                    temp.adjacentTo.remove(nodeToRemove)
+
+                    # nw
+                    if (row > 0 and graph[col][row - 1].walls[directions.WEST]):
+                        nodeToRemove = graph[col-1][row-1]
+                        temp.adjacentTo.remove(nodeToRemove)
+
+                    # sw
+                    if (row < 15 and graph[col][row + 1].walls[directions.WEST]):
+                        nodeToRemove = graph[col-1][row+1]
+                        temp.adjacentTo.remove(nodeToRemove)
+
+
+
+def printAdj(col, row):
+    temp = graph[col][row]
+    print("Adj To - - - " + temp.name)
+    for node in temp.adjacentTo:
+        print(node.name, end=', ')
+    print()
+
+
+# Nothing but side effects, baby. Quality programming here.
 buildGraph()
 addTerrain()
 addWalls()
 
+printAdj(1, 9)
+removeWallAdjacencies()
+printAdj(1, 9)
+
+'''
 for i in range(cols):
     for j in range(rows):
         print("Row: " + str(j) + "   Col:" + str(i))
@@ -94,6 +381,7 @@ for i in range(cols):
           print(k.name,end=', ')
         print('     ' + node.terrain)
         print('     ' + str(node.walls))
+'''
 
 
 
